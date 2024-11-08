c3bb9885791c578af419f068c66105bd05f13c6f
Armando Valdez <valde148@d.umn.edu>
2024-11-07 21:33:32 -0600

    testing

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -12,22 +12,28 @@ public class EchoClient {
         EchoClient client = new EchoClient();
         client.start();
     }
-
+	//it will start with a clinet and takes care of data and tranvels between the server
     private void start() throws IOException {
         Socket socket = new Socket("localhost", PORT_NUMBER);
         InputStream socketInputStream = socket.getInputStream();
         OutputStream socketOutputStream = socket.getOutputStream();
+		//will send the data from system.in into the server
 
         goIntoServer(System.in, socketOutputStream);
 
+		//will tell the server that all info has been sent
         socket.shutdownOutput();
-
+		//will receive data from server and system.out
         gotByServer(socketInputStream, System.out);
-
+		//will close the socket
         socket.close();
         socketInputStream.close();
+
+
         socketOutputStream.close();
     }
+
+	//it will read from the steam input and writes the output steam
 
     private void goIntoServer(InputStream input, OutputStream output) throws IOException {
     	for (int byteData = input.read(); byteData != -1; byteData = input.read()) {
@@ -35,7 +41,7 @@ public class EchoClient {
     }
     output.flush(); 
 }
-
+	//it reads the data and flushes the output
 	private void gotByServer(InputStream input, OutputStream output) throws IOException {
     	for (int byteData = input.read(); byteData != -1; byteData = input.read()) {
         	output.write(byteData);
