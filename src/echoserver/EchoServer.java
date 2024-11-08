e65d692a642f7793eda172dac448e44fd653e4b3
Armando Valdez <valde148@d.umn.edu>
2024-11-07 20:30:41 -0600

    doign server

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -7,25 +7,19 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-	
-	// REPLACE WITH PORT PROVIDED BY THE INSTRUCTOR
-	public static final int PORT_NUMBER = 0; 
-	public static void main(String[] args) throws IOException, InterruptedException {
-		EchoServer server = new EchoServer();
-		server.start();
-	}
+    public static final int PORT_NUMBER = 6013;
+    public static void main(String[] args) throws IOException, InterruptedException {
+        EchoServer server = new EchoServer();
+        server.start();
+    }
 
-	private void start() throws IOException, InterruptedException {
-		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
-		while (true) {
-			Socket socket = serverSocket.accept();
-
-			// Put your code here.
-			// This should do very little, essentially:
-			//   * Construct an instance of your runnable class
-			//   * Construct a Thread with your runnable
-			//      * Or use a thread pool
-			//   * Start that thread
-		}
-	}
+    private void start() throws IOException, InterruptedException {
+        ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            Thread clientHandler = new Thread(() -> handleClient(clientSocket));
+            clientHandler.start();
+        }
+    }
+  
 }
